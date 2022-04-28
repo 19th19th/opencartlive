4ddbd128d5609062533e905aecd82c91df5223b5
unishop2 <50132185+unishop2@users.noreply.github.com>
2022-04-28 11:43:06 +0400

    translate upd

--- upload/admin/language/ru-ru/common/header.php
+++ upload/admin/language/ru-ru/common/header.php
@@ -1,20 +1,12 @@
 <?php
-
 // Heading
-$_['heading_title']      	 = 'ocStore';
+$_['heading_title']      = 'Администрирование';
 
 // Text
-$_['text_profile']      		  = 'Ваш профиль';
-$_['text_store']           	 	  = 'Магазины';
-$_['text_help']            	 	  = 'Помощь';
-$_['text_homepage']        	 	  = 'Сайт проекта';
-$_['text_support']         	 	  = 'Форум поддержки';
-$_['text_documentation']   	 	  = 'Документация';
-$_['text_logout']          	 	  = 'Выход';
-$_['text_search_options']  		  = 'Опции поиска';
-$_['text_new']  		   		  = 'Добавить';
-$_['text_new_category']    		  = 'Категорию';
-$_['text_new_customer']    		  = 'Пользователя';
-$_['text_new_download']           = 'Загрузку';
-$_['text_new_manufacturer']		  = 'Производителя';
-$_['text_new_product']     		  = 'Товар';
+$_['text_profile']       = 'Ваш профиль';
+$_['text_store']         = 'Магазин';
+$_['text_help']          = 'Помощь';
+$_['text_homepage']      = 'Сайт ';
+$_['text_support']       = 'Форум';
+$_['text_documentation'] = 'Документация';
+$_['text_logout']        = 'Выход';
