42a6b90030ab5cf84ee70230bb32fc32d1123495
unishop2 <50132185+unishop2@users.noreply.github.com>
2022-04-28 11:32:26 +0400

    add translate

---
+++ upload/catalog/language/ru-ru/extension/captcha/basic.php
@@ -0,0 +1,10 @@
+<?php
+
+// Text
+$_['text_captcha']  = 'Капча';
+
+// Entry
+$_['entry_captcha'] = 'Введите код в поле ниже';
+
+// Error
+$_['error_captcha'] = 'Неверно введен код с картинки!';
---
+++ upload/catalog/language/ru-ru/extension/module/featured_article.php
@@ -0,0 +1,9 @@
+<?php
+
+// Heading
+$_['heading_title'] = 'Рекомендуем почитать';
+$_['text_views'] 	= 'Просмотров:';
+$_['button_more']   = 'подробнее';
+
+// Text
+$_['text_tax']      = 'Без НДС:';
